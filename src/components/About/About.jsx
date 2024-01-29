edb4b1c5af1c1bb2dd6d48144f3aaf76317a28b9
Nathalie <arnouxn@gmail.com>
2024-01-30 00:31:48 +0100

    figured how to center hori and verti, added content for tests

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,13 @@
 import './About.scss';
 
 const About = () => {
-  return <p className="content">About</p>;
+  return (
+    <div className="About">
+      <h2 className="About-title">About</h2>
+      <p>This is gonna be about me!</p>
+      <p>And this will be too :D</p>
+    </div>
+  );
 };
 
 export default About;
