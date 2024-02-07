8c79511f091dcd25efb26c3b9b5c375a410b99b4
Nathalie <arnouxn@gmail.com>
2024-02-07 18:56:31 +0100

    started writing intro ans skills sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,32 +6,165 @@ const About = () => {
       <h2 className="About-title">About</h2>
       <ul>
         <li>
-          Start with a brief introduction: Who you are, your background, and
-          what sparked your interest in web development.
+          <h3>
+            Start with a brief introduction: Who you are, your background, and
+            what sparked your interest in web development.
+          </h3>
+          <div>
+            <p>
+              Hello there! I&apos;m Nathalie, a junior web developer with a
+              passion for crafting creative digital experiences. I have a
+              college diploma (licence) in natural languages but have recently
+              decided to start exploring the world of web development. Indeed,
+              since childhood, I&apos;ve been captivated by the seemingly
+              contrasting realms of technology and creativity. This fascination
+              has propelled me into the dynamic world of web development, where
+              creativity and technology intertwine to solve problems and create
+              new experiences for the users. And I love it!
+            </p>
+          </div>
         </li>
         <li>
-          Highlight your skills: Mention the programming languages, frameworks,
-          and tools you&apos;re proficient in.
+          <h3>
+            Highlight your skills: Mention the programming languages,
+            frameworks, and tools you&apos;re proficient in.
+          </h3>
+          <div>
+            <p>
+              During my immersive six-month training program with O&apos;Clock,
+              I acquired a comprehensive skill set that has equipped me to
+              navigate the ever-evolving ocean of web development. This
+              intensive experience, culminating in the attainment of a
+              professional title recognized by the state, provided me with
+              hands-on training (include link to Webstellar) and theoretical
+              knowledge in a wide array of technologies and methodologies.
+            </p>
+            <h4>Languages and Frameworks:</h4>
+            <ul>
+              <li>
+                <h5>HTML, CSS, JavaScript:</h5>
+                <p>
+                  These foundational languages are the bedrock of web
+                  development. Despite their apparent simplicity, they possess
+                  hidden depths and complexities, empowering developers to craft
+                  visually appealing and interactive user interfaces.
+                </p>
+              </li>
+              <li>
+                <h5>PHP:</h5>
+                <p>
+                  Despite its reputation, it is omnipresent in web development,
+                  enabling the seamless creation of dynamic web applications
+                  with robust backend systems.
+                </p>
+              </li>
+              <li>
+                <h5>SQL:</h5>
+                <p>
+                  Essential for designing and managing relational databases, it
+                  ensures efficient data storage and retrieval, laying the
+                  groundwork for scalable and organized data management.
+                </p>
+              </li>
+              <li>
+                <h5>React:</h5>
+                <p>
+                  Probably my favorite so far, it enables the creation of
+                  modern, responsive, and user-friendly web applications.
+                  It&apos;s an incredible tool for expressing creativity through
+                  code.
+                </p>
+              </li>
+              <li>
+                <h5>Laravel and Symfony:</h5>
+                <p>
+                  Renowned PHP frameworks, they streamline development processes
+                  and build robust, scalable web solutions efficiently.
+                </p>
+              </li>
+            </ul>
+            <h4>Tools and Methodologies:</h4>
+            <ul>
+              <li>
+                <h5>Git and GitHub:</h5>
+                <p>
+                  Essential for version control in web development, Git and
+                  GitHub facilitate collaboration with teams and efficient
+                  management of project repositories.
+                </p>
+              </li>
+              <li>
+                <h5>Yarn:</h5>
+                <p>
+                  Renowned package manager, it optimizes project dependencies
+                  and streamlines development workflows, enhancing productivity.
+                </p>
+              </li>
+              <li>
+                <h5>Adminer, phpMyAdmin:</h5>
+                <p>
+                  Used for creating and managing relational databases, they
+                  ensure data integrity and facilitate efficient database
+                  operations.
+                </p>
+              </li>
+              <li>
+                <h5>Agile Methodology:</h5>
+                <p>
+                  Embracing iterative development practices, this method fosters
+                  collaboration, flexibility, and continuous improvement,
+                  enhancing project adaptability and efficiency.
+                </p>
+              </li>
+              <li>
+                <h5>Wireframing:</h5>
+                <p>
+                  Enables visualization of project layouts and user flows,
+                  facilitating effective communication and design planning to
+                  ensure alignment with project objectives and user needs.
+                </p>
+              </li>
+              <li>
+                <h5>Website Deployment:</h5>
+                <p>
+                  Transition from development to production environments, it
+                  employs various strategies to ensure seamless and efficient
+                  deployment processes, enhancing project scalability and
+                  accessibility.
+                </p>
+              </li>
+            </ul>
+          </div>
         </li>
         <li>
-          Share your passion: Talk about what excites you most in web
-          development and any particular areas of expertise.
+          <h3>
+            Share your passion: Talk about what excites you most in web
+            development and any particular areas of expertise.
+          </h3>
         </li>
         <li>
-          Discuss your experience: If you have relevant work or educational
-          experience, mention it here.
+          <h3>
+            Discuss your experience: If you have relevant work or educational
+            experience, mention it here.
+          </h3>
         </li>
         <li>
-          Showcase your values: Share your approach to web development, your
-          work ethic, and any principles that guide your work.
+          <h3>
+            Showcase your values: Share your approach to web development, your
+            work ethic, and any principles that guide your work.
+          </h3>
         </li>
         <li>
-          Conclude with a call to action: Encourage visitors to explore your
-          projects, resume, and get in touch.
+          <h3>
+            Conclude with a call to action: Encourage visitors to explore your
+            projects, resume, and get in touch.
+          </h3>
         </li>
         <li>
-          If you have received positive feedback from clients or collaborators,
-          you could include a section for testimonials.
+          <h3>
+            If you have received positive feedback from clients or
+            collaborators, you could include a section for testimonials.
+          </h3>
         </li>
       </ul>
     </div>
