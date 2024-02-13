b67056baa848ac8e2887a05e3982f2da13a1700a
Nathalie <arnouxn@gmail.com>
2024-02-13 18:12:29 +0100

    created hash links with library, moved data to external file and mapped it to be displayed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,209 +1,56 @@
+import { NavHashLink } from 'react-router-hash-link';
 import './About.scss';
+
+import aboutData from './About_data';
+
+// const tabs = ['about', 'resume', 'projects', 'contact'];
+
+/* const sections = [
+  { name: 'Intro', hash: 'intro' },
+  { name: 'Skills', hash: 'skills' },
+  { name: 'Interest', hash: 'interest' },
+  { name: 'Soft Skills', hash: 'softSkills' },
+  { name: 'Values', hash: 'values' },
+]; */
+
+const ContentNav = () => {
+  return (
+    <ul className="About-nav">
+      {aboutData.map((section) => {
+        return (
+          <li key={section.hash}>
+            <NavHashLink to={`/about#${section.hash}`}>
+              {section.name}
+            </NavHashLink>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
 const About = () => {
   return (
     <div className="About">
       <h2 className="About-title">About</h2>
+      <ContentNav />
+      <img className="About-img" src="src\assets\old\Placeholder.png" alt="" />
       <ul>
-        <li>
-          <h3>
-            Start with a brief introduction: Who you are, your background, and
-            what sparked your interest in web development.
-          </h3>
-          <div>
-            <p>
-              Hello there! I&apos;m Nathalie, a junior web developer with a
-              passion for crafting creative digital experiences. I have a
-              college licence in languages (English & Japanese) but have
-              recently decided to reinvent myself and started exploring the
-              fabulous world of web development.
-            </p>
-          </div>
-        </li>
-        <li>
-          <h3>
-            Highlight your skills: Mention the programming languages,
-            frameworks, and tools you&apos;re proficient in.
-          </h3>
-          <div>
-            <p>
-              During my immersive six-month training program with O&apos;Clock,
-              I acquired a comprehensive skill set that has equipped me to
-              navigate the ever-evolving ocean of web development. This
-              intensive experience, culminating in the attainment of a
-              professional title recognized by the state, provided me with
-              hands-on training (include link to Webstellar) and theoretical
-              knowledge in a wide array of technologies and methodologies.
-            </p>
-            <h4>Languages and Frameworks:</h4>
-            <ul>
-              <li>
-                <h5>HTML, CSS, JavaScript:</h5>
-                <p>
-                  These foundational languages are the bedrock of web
-                  development. Despite their apparent simplicity, they possess
-                  hidden depths and complexities, empowering developers to craft
-                  visually appealing and interactive user interfaces.
-                </p>
-              </li>
-              <li>
-                <h5>PHP:</h5>
-                <p>
-                  Despite its reputation, it is omnipresent in web development,
-                  enabling the seamless creation of dynamic web applications
-                  with robust backend systems.
-                </p>
-              </li>
-              <li>
-                <h5>SQL:</h5>
-                <p>
-                  Essential for designing and managing relational databases, it
-                  ensures efficient data storage and retrieval, laying the
-                  groundwork for scalable and organized data management.
-                </p>
-              </li>
-              <li>
-                <h5>React:</h5>
-                <p>
-                  Probably my favorite so far, it enables the creation of
-                  modern, responsive, and user-friendly web applications.
-                  It&apos;s an incredible tool for expressing creativity through
-                  code.
-                </p>
-              </li>
-              <li>
-                <h5>Laravel and Symfony:</h5>
-                <p>
-                  Renowned PHP frameworks, they streamline development processes
-                  and build robust, scalable web solutions efficiently.
-                </p>
-              </li>
-            </ul>
-            <h4>Tools and Methodologies:</h4>
-            <ul>
-              <li>
-                <h5>Git and GitHub:</h5>
-                <p>
-                  Essential for version control in web development, Git and
-                  GitHub facilitate collaboration with teams and efficient
-                  management of project repositories.
-                </p>
-              </li>
-              <li>
-                <h5>Yarn:</h5>
-                <p>
-                  Renowned package manager, it optimizes project dependencies
-                  and streamlines development workflows, enhancing productivity.
-                </p>
-              </li>
-              <li>
-                <h5>Adminer, phpMyAdmin:</h5>
-                <p>
-                  Used for creating and managing relational databases, they
-                  ensure data integrity and facilitate efficient database
-                  operations.
-                </p>
-              </li>
-              <li>
-                <h5>Agile Methodology:</h5>
-                <p>
-                  Embracing iterative development practices, this method fosters
-                  collaboration, flexibility, and continuous improvement,
-                  enhancing project adaptability and efficiency.
-                </p>
-              </li>
-              <li>
-                <h5>Wireframing:</h5>
-                <p>
-                  Enables visualization of project layouts and user flows,
-                  facilitating effective communication and design planning to
-                  ensure alignment with project objectives and user needs.
-                </p>
-              </li>
-              <li>
-                <h5>Website Deployment:</h5>
-                <p>
-                  Transition from development to production environments, it
-                  employs various strategies to ensure seamless and efficient
-                  deployment processes, enhancing project scalability and
-                  accessibility.
-                </p>
-              </li>
-            </ul>
-          </div>
-        </li>
-        <li>
-          <h3>
-            Share your passion: Talk about what excites you most in web
-            development and any particular areas of expertise.
-          </h3>
-          <p>
-            From a young age, I&apos;ve been fascinated by the two seemingly
-            contrasting realms of technology and creativity. This fascination
-            has recently led me into the dynamic realm of web development, where
-            I find joy in blending creativity with technical prowess to solve
-            problems and craft captivating digital experiences. I&apos;m
-            particularly drawn to the expressive capabilities of React and CSS.
-            These technologies empower me to design attractive and intuitively
-            navigable user interfaces, providing users with seamless and
-            memorable interactions.
-          </p>
-        </li>
-        <li>
-          <h3>
-            [Soft Skills] Discuss your experience: If you have relevant work or
-            educational experience, mention it here.
-          </h3>
-          <p>
-            I hold a college degree in languages, specializing in English and
-            Japanese applied to business. This educational background has
-            equipped me with near-fluency in English and a nuanced understanding
-            of intercultural communication, enabling me to effectively
-            collaborate with diverse teams and clients. As part of my degree, I
-            completed a short internship at a news agency specialized in
-            Japanese news. Here, I gained valuable insights into search engine
-            optimization (SEO) and had the opportunity to apply the knowledge of
-            competitive intelligence and market research acquired during my
-            college years. This experience solidified the importance of
-            strategic thinking and data-driven decision-making in digital
-            marketing. My diverse professional experiences have underscored the
-            significance of effective communication, respect for hierarchy, and
-            the cultivation of teamwork. I thrive in collaborative environments,
-            leveraging collective strengths to achieve shared objectives. I
-            sincerely believe in the adage, &quot;If you want to go fast, go
-            alone. If you want to go far, go together.&quot; During my recent
-            web development training program, I was honored to be selected as
-            the class representative. I was commended by the program&apos;s
-            tutor for my role in fostering a cohesive and dynamic learning
-            environment, showcasing my dedication to supporting and encouraging
-            my peers. At the age of 26, I was offered the opportunity to undergo
-            managerial training, a testament to my seriousness and rapid
-            learning abilities. While I ultimately chose to pursue other paths,
-            this experience granted me insights into higher-level
-            decision-making processes. It equipped me with a deeper
-            understanding of leadership dynamics, delegation strategies, and
-            decision-making, enabling me to understand and respect managerial
-            decisions in a deeper way.
-          </p>
-        </li>
-        <li>
-          <h3>
-            Showcase your values: Share your approach to web development, your
-            work ethic, and any principles that guide your work.
-          </h3>
-        </li>
-        <li>
-          <h3>
-            Conclude with a call to action: Encourage visitors to explore your
-            projects, resume, and get in touch.
-          </h3>
-        </li>
-        <li>
-          <h3>
-            If you have received positive feedback from clients or
-            collaborators, you could include a section for testimonials.
-          </h3>
-        </li>
+        {aboutData.map((section) => {
+          return (
+            <li key={section.hash}>
+              <h3 id={section.hash}>{section.name}</h3>
+              <div>
+                {section.content.map((content) => {
+                  const Tag = content.tag;
+                  return <Tag key={content.id}>{content.text}</Tag>;
+                })}
+              </div>
+            </li>
+          );
+        })}
+        {/* Conclude with a call to action: Encourage visitors to explore your projects, resume, and get in touch.
+        If you have received positive feedback from clients or collaborators, you could include a section for testimonials. */}
       </ul>
     </div>
   );
---
+++ src/components/About/About_data.js
@@ -0,0 +1,121 @@
+const aboutData = [
+  {
+    name: 'Intro',
+    hash: 'intro',
+    content: [
+      {
+        id: 'intro',
+        tag: 'p',
+        text: "Hello there! I'm Nathalie, a junior web developer with a passion for crafting creative digital experiences. I have a college licence in languages (English & Japanese) but have recently decided to reinvent myself and started exploring the fabulous world of web development.",
+      },
+    ],
+  },
+  {
+    name: 'Skills',
+    hash: 'skills',
+    content: [
+      {
+        id: 'intro',
+        tag: 'p',
+        text: "During my immersive six-month training program with O'Clock, I acquired a comprehensive skill set that has equipped me to navigate the ever-evolving ocean of web development. This intensive experience, culminating in the attainment of a professional title recognized by the state, provided me with hands-on training (include link to Webstellar) and theoretical knowledge in a wide array of technologies and methodologies.",
+      },
+      {
+        id: 'languages',
+        tag: 'h4',
+        text: 'Languages, Libraries and Frameworks:',
+      },
+      {
+        id: 'htmlCssJs',
+        tag: 'p',
+        text: 'HTML, CSS, JavaScript: These foundational languages are the bedrock of web development. Despite their apparent simplicity, they possess hidden depths and complexities, empowering developers to craft visually appealing and interactive user interfaces.',
+      },
+      {
+        id: 'php',
+        tag: 'p',
+        text: 'PHP: Despite its reputation, it is omnipresent in web development, enabling the seamless creation of dynamic web applications with robust backend systems.',
+      },
+      {
+        id: 'sql',
+        tag: 'p',
+        text: 'SQL: Essential for designing and managing relational databases, it ensures efficient data storage and retrieval, laying the groundwork for scalable and organized data management.',
+      },
+      {
+        id: 'react',
+        tag: 'p',
+        text: "React: Probably my favorite so far, it enables the creation of modern, responsive, and user-friendly web applications. It's an incredible tool for expressing creativity through code.",
+      },
+      {
+        id: 'laravelSymfony',
+        tag: 'p',
+        text: 'Laravel and Symfony: Renowned PHP frameworks, they streamline development processes and build robust, scalable web solutions efficiently.',
+      },
+      {
+        id: 'tools',
+        tag: 'h4',
+        text: 'Tools and Methodologies:',
+      },
+      {
+        id: 'gitGithub',
+        tag: 'p',
+        text: 'Git and GitHub: Essential for version control in web development, Git and GitHub facilitate collaboration with teams and efficient management of project repositories.',
+      },
+      {
+        id: 'yarn',
+        tag: 'p',
+        text: 'Yarn: Renowned package manager, it optimizes project dependencies and streamlines development workflows, enhancing productivity.',
+      },
+      {
+        id: 'adminerPhpmyadmin',
+        tag: 'p',
+        text: 'Adminer, phpMyAdmin: Used for creating and managing relational databases, they ensure data integrity and facilitate efficient database operations.',
+      },
+      {
+        id: 'agile',
+        tag: 'p',
+        text: 'Agile Methodology: Embracing iterative development practices, this method fosters collaboration, flexibility, and continuous improvement, enhancing project adaptability and efficiency.',
+      },
+      {
+        id: 'wireframing',
+        tag: 'p',
+        text: 'Wireframing: Enables visualization of project layouts and user flows, facilitating effective communication and design planning to ensure alignment with project objectives and user needs.',
+      },
+      {
+        id: 'deployment',
+        tag: 'p',
+        text: 'Website Deployment: Transition from development to production environments, it employs various strategies to ensure seamless and efficient deployment processes, enhancing project scalability and accessibility.',
+      },
+    ],
+  },
+  {
+    name: 'Passion',
+    hash: 'passion',
+    content: [
+      {
+        tag: 'p',
+        text: "From a young age, I've been fascinated by the two seemingly contrasting realms of technology and creativity. This fascination has recently led me into the dynamic realm of web development, where I find joy in blending creativity with technical prowess to solve problems and craft captivating digital experiences. I'm particularly drawn to the expressive capabilities of React and CSS. These technologies empower me to design attractive and intuitively navigable user interfaces, providing users with seamless and memorable interactions.",
+      },
+    ],
+  },
+  {
+    name: 'Soft Skills',
+    hash: 'softSkills',
+    content: [
+      {
+        tag: 'p',
+        text: "I hold a college degree in languages,specializing in English and Japanese applied to business. This educational background has equipped me with near-fluency in English and a nuanced understanding of intercultural communication, enabling me to effectively collaborate with diverse teams and clients. As part of my degree, I completed a short internship at a news agency specialized in Japanese news. Here, I gained valuable insights into search engine optimization (SEO) and had the opportunity to apply the knowledge of competitive intelligence and market research acquired during my college years. This experience solidified the importance of strategic thinking and data-driven decision-making in digital marketing. My diverse professional experiences have underscored the significance of effective communication, respect for hierarchy, and the cultivation of teamwork. I thrive in collaborative environments, leveraging collective strengths to achieve shared objectives. I sincerely believe in the adage, 'If you want to go fast, go alone. If you want to go far, go together'. During my recent web development training program, I was honored to be selected as the class representative. I was commended by the program's tutor for my role in fostering a cohesive and dynamic learning environment, showcasing my dedication to supporting and encouraging my peers. At the age of 26, I was offered the opportunity to undergo managerial training, a testament to my seriousness and rapid learning abilities. While I ultimately chose to pursue other paths, this experience granted me insights into higher-level decision-making processes. It equipped me with a deeper understanding of leadership dynamics, delegation strategies, and decision-making, enabling me to understand and respect managerial decisions in a deeper way.",
+      },
+    ],
+  },
+  {
+    name: 'Values',
+    hash: 'values',
+    content: [
+      {
+        tag: 'p',
+        text: 'Showcase your values: Share your approach to web development, your work ethic, and any principles that guide your work.',
+      },
+    ],
+  },
+];
+
+export default aboutData;
